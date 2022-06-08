7f6a158b6d4f3c2e709f3c6f593ad575b257915e
GitExample <git@example.com>
2022-06-08 19:58:26 +0530

    .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,12 @@
 ;; Directory loacl variable for roam
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((eval . (setq-local org-roam-directory (expand-file-name "./")))
-(eval . (git-auto-commit-mode 1))
-(org-attach-id-dir . "~/roam/attachments")
-)))
+((nil . ((eval . (setq-local
+                  org-roam-directory (expand-file-name (locate-dominating-file
+                                                        default-directory ".dir-locals.el"))))
+         (eval . (setq-local
+                  org-roam-db-location (expand-file-name "org-roam.db"
+                                                         org-roam-directory)))
+         (eval . (git-auto-commit-mode 1))
+         (org-attach-id-dir . "~/roam/attachments")
+         )))
